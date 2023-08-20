376339ec58eb608383d1e5ca4902c4336ee52ab0
Andrey Solodov <solodov.dev@gmail.com>
2023-08-20 15:42:15 +0300

    Rust day 5

--- langs/rust/src/lib.rs
+++ langs/rust/src/lib.rs
@@ -1,3 +1,4 @@
+use fancy_regex::Regex;
 use std::{
     fmt::Display,
     io::{self, BufRead},
@@ -10,4 +11,8 @@ pub fn run<T: Display>(f: fn(&str) -> T) {
         .fold("".to_string(), |acc, line| acc + &line.unwrap() + "\n");
 
     println!("{}", f(input.trim()));
+}
+
+pub fn check(regex: &str, str: &str) -> bool {
+    Regex::new(regex).unwrap().is_match(str).unwrap()
 }
